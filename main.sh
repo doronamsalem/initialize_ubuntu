103681d4e46ea41e1f96c6edfbefc6461f6860e5
doron amsalem <dorona3@rafael.co.il>
2023-02-28 13:28:30 +0200

    add installations for softwares

--- main.sh
+++ main.sh
@@ -1,9 +1,13 @@
+#will be activate every terminal session, file path must match ~/.bashrc "source" command
 #!/bin/bash
-#will be activate every terminal session, file path must match ~/.bashrc conntent
+
+first_initialize=false #for software installations change to true
+export PATH_INITIALIZE_UBUNTU=/home/dorona3/Desktop/initialize_ubuntu
 
 #useful aliases
 alias c='clear'
 alias s='sudo'
+alias k='kubectl'
 
 #git shortcut
 alias ga='git add'
@@ -11,9 +15,13 @@ alias gc='git commit -m'
 alias gp='git push'
 alias gs='git status'
   #auto git add, commit, push script
-  lazyManWork=$(find ~ -type f -name git_shortcut.sh)
+  lazyManWork=$(find $PATH_INITIALIZE_UBUNTU -type f -name git_shortcut.sh)
   alias gitit='source ${lazyManWork}'
 
 #change layout script bind to f10 key
-changeLayout=$(find ~ -type f -name change_layout.sh)
+changeLayout=$(find $PATH_INITIALIZE_UBUNTU -type f -name change_layout.sh)
 bind '"\e[21~":"source ${changeLayout}\n"'
+
+#softwares installations
+softwares_installations=$(find $PATH_INITIALIZE_UBUNTU -type f -name softwares.sh)
+source ${softwares_installations}
