9abce43f41628bc977a615bfdf6811849b6ca9de
doron amsalem <dorona3@rafael.co.il>
2023-05-31 18:02:55 +0300

    add installtion of ansible and terraform

--- scripts/softwares.sh
+++ scripts/softwares.sh
@@ -6,7 +6,7 @@ if $first_initialize
 then
     sed -i 's/export first_initialize=true/export first_initialize=false/' $COUSTOM_CONFIGURATION_DIR/main.sh
     
-    softwares=("helm", "kubectl", "docker")
+    softwares=("helm", "kubectl", "docker", "python3", "ansible", "terraform)
     apps=("vscode", "whatsapp", "telegram", "google chrome", "zoom")
     echo -e "intallations file path" `pwd` "\n"
 
@@ -14,10 +14,10 @@ then
     read confermation
     if [[ "$confermation" == "y" || "$confermation" == "Y" ]]
     then
-        #snap 
+        #snap
             sudo apt update -y
             sudo apt install snapd -y
-        #helm 
+        #helm
             curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
             sudo apt-get install apt-transport-https --yes
             echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
@@ -30,7 +30,14 @@ then
             sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
         #docker
             sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+	#python3
+	    sudo apt-get install python3 -y
+	#ansible
+	    python3 -m pip install --user ansible -y
+	#terraform
+	    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	    sudo apt install terraform -y
     fi
 
     echo -ne "Do you want to install:\n${apps[@]}\n[y/n]\n"
